37cf7f9ef7223562319bcc9a25c82c5672bc3195
guanidine <zhang010725@gmail.com>
2023-03-15 13:25:33 +0800

    YOLOv1: just for test

---
+++ YOLOv1/README.md
@@ -0,0 +1,1 @@
+https://www.cv-foundation.org/openaccess/content_cvpr_2016/html/Redmon_You_Only_Look_CVPR_2016_paper.html
---
+++ YOLOv1/loss.py
@@ -0,0 +1,96 @@
+import torch
+from torch import nn
+
+from metrics.iou import intersection_over_union
+
+
+class YoloLoss(nn.Module):
+    def __init__(self, split_size=7, num_boxes=2, num_classes=20):
+        super(YoloLoss, self).__init__()
+        self.mse = nn.MSELoss(reduction="sum")
+        self.split_size = split_size
+        self.num_boxes = num_boxes
+        self.num_classes = num_classes
+        self.lambda_noobj = 0.5
+        self.lambda_coord = 5
+
+    def forward(self, predictions, target):
+        # N x S*S(C+B*5) -> N x S x S x C+B*5
+        predictions = predictions.reshape(-1, self.split_size, self.split_size, self.num_classes + self.num_boxes * 5)
+
+        iou_b1 = intersection_over_union(predictions[..., 21:25], target[..., 21:25])
+        iou_b2 = intersection_over_union(predictions[..., 26:30], target[..., 21:25])
+        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
+        iou_maxes, best_box = torch.max(ious, dim=0)
+        exists_box = target[..., 20].unsqueeze(3)  # Iobj_i
+
+        # ======================== #
+        #   FOR BOX COORDINATES    #
+        # ======================== #
+
+        # Set boxes with no object in them to 0. We only take out one of the two
+        # predictions, which is the one with highest Iou calculated previously.
+        box_predictions = exists_box * (
+            (
+                    best_box * predictions[..., 26:30]
+                    + (1 - best_box) * predictions[..., 21:25]
+            )
+        )
+        box_targets = exists_box * target[..., 21:25]
+
+        # Take sqrt of width, height of boxes
+        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(
+            torch.abs(box_predictions[..., 2:4] + 1e-6)
+        )
+        box_targets[..., 2:4] = torch.sqrt(box_targets[..., 2:4])
+
+        box_loss = self.mse(
+            torch.flatten(box_predictions, end_dim=-2),
+            torch.flatten(box_targets, end_dim=-2)
+        )
+
+        # ==================== #
+        #   FOR OBJECT LOSS    #
+        # ==================== #
+
+        # pred_box is the confidence score for the bbox with highest IoU
+        pred_box = (best_box * predictions[..., 25:26] + (1 - best_box) * predictions[..., 20:21])
+
+        object_loss = self.mse(
+            torch.flatten(exists_box * pred_box),
+            torch.flatten(exists_box * target[..., 20:21])
+        )
+
+        # ======================= #
+        #   FOR NO OBJECT LOSS    #
+        # ======================= #
+
+        # (N, S, S, 1) -> (N, S*S)
+        no_object_loss = self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., 20:21], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1)
+        )
+
+        no_object_loss += self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., 25:26], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1)
+        )
+
+        # ================== #
+        #   FOR CLASS LOSS   #
+        # ================== #
+
+        # (N, S, S, 20) -> (N*S*S, 20)
+        class_loss = self.mse(
+            torch.flatten(exists_box * predictions[..., :20], end_dim=-2),
+            torch.flatten(exists_box * target[..., :20], end_dim=-2)
+        )
+
+        loss = (
+                self.lambda_coord * box_loss  # first two rows
+                + object_loss  # third row
+                + self.lambda_noobj * no_object_loss  # forth row
+                + class_loss  # fifth row
+        )
+
+        return loss
